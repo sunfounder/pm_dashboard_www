6177fb26d4aebcdcb75903321608396d6237479d
Cavon Lee <cavonxx@gmail.com>
2024-02-29 17:29:55 +0800

    fix storage card detail display

--- src/components/cards/storageCard.jsx
+++ src/components/cards/storageCard.jsx
@@ -7,7 +7,6 @@ import { useTheme } from '@mui/material/styles';
 const StorageCard = (props) => {
   const theme = useTheme();
   let detail = {};
-  let newData = [];
   let storageCardData = null;
   let diskData = [];
   if (props.data.length >= 2) {
@@ -31,35 +30,29 @@ const StorageCard = (props) => {
     });
   }
   // 重组 detail 和 newData 数据
+  let newDataEntry = {};
+  let newDiskData = []
   diskData.forEach(entry => {
-    let dataKey = entry.type;
-    detail[dataKey] = {
-      title: entry.type,
+    let diskName = entry.type;
+    if (entry.total === 0) return;
+    newDiskData.push(entry);
+    detail[diskName] = {
+      title: diskName,
       unit: ""
     };
-    let newDataEntry = {};
-    // newDataEntry[dataKey] = `${formatBytes(entry.used)}B / ${formatBytes(entry.total)}B`;
-    newDataEntry[dataKey] = `${entry.percent}%`;
-    newData.push(newDataEntry);
+    newDataEntry[diskName] = `${entry.percent}%`;
   });
   return (
     <Card
       color="storage"
       title="Storage"
       width={4}
-      data={newData}
+      data={[newDataEntry]}
       details={detail}
       iconBoxColor={theme.palette.storage.main}
       processorChartAmount={true}
       chart={
-        // <div className='multipleChartsBox'>
-        //   <div className='chartBox' style={{ gridTemplateColumns: chartNumber === 2 ? "repeat(2, 1fr)" : `repeat(${chartNumber / 2}, 1fr)` }}>
-        //     {/* {Array.from({ length: 2 }).map((_, index) => (
-        //       <PieChart key={index} detail={detail} data={mmcblk0} processorChartAmount={true} />
-        //     ))} */}
-        //   </div>
-        // </div>
-        <BarChart color="storage" detail={detail} data={diskData} />
+        <BarChart color="storage" detail={detail} data={newDiskData} />
       }
       icon={<svg aria-hidden="true" focusable="false" height="2em" fill={theme.palette.iconFg.main} data-prefix="fas" data-icon="hard-drive" class="svg-inline--fa fa-hard-drive fa-2x " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V280.4c-17-15.2-39.4-24.4-64-24.4H64c-24.6 0-47 9.2-64 24.4V96zM64 288H448c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V352c0-35.3 28.7-64 64-64zM320 416a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm128-32a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg>}
       pieChart={true}
